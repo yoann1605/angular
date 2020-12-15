fdc6b6f4fa665ba5a0ad3627d234dcf3dffea95d
mlima95 <milan.lima@hotmail.fr>
2020-12-15 19:24:39 +0100

    [Init] - Add bootstrap and start project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule} from "@angular/router";
+import { ListImageComponent } from './list-image/list-image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListImageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: ListImageComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/list-image/list-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-image',
+  templateUrl: './list-image.component.html',
+  styleUrls: ['./list-image.component.css']
+})
+export class ListImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
